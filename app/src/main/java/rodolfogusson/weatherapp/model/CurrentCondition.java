375e76fc12bb4c2fc1010c588a5aa6bf1c00a26f
Rodolfo Gusson <rodolfoggp@gmail.com>
2017-05-17 10:20:15 -0300

    Initial commit

---
+++ app/src/main/java/rodolfogusson/weatherapp/model/CurrentCondition.java
@@ -0,0 +1,8 @@
+package rodolfogusson.weatherapp.model;
+
+/**
+ * Created by rodolfo on 5/16/17.
+ */
+
+public class CurrentCondition {
+}
---
+++ app/src/main/java/rodolfogusson/weatherapp/model/JSONWeatherParser.java
@@ -0,0 +1,9 @@
+package rodolfogusson.weatherapp.model;
+
+/**
+ * Created by rodolfo on 5/16/17.
+ */
+
+public class JSONWeatherParser {
+
+}
